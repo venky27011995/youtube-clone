0684025b45fc735635e7a46d575160ab04212a8b
Venkatesh Ananthoju <49225539+venky27011995@users.noreply.github.com>
2022-02-16 09:45:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/header/Header'
+import Sidebar from './components/sidebar/Sidebar'
+import HomeScreen from './screens/homeScreen/HomeScreen'
+import './_app.scss'
 
-function App() {
+const App = () => {
+  const [sidebarToggle,SetSidebarToggle] = useState(true);
+
+  const handleSidebarToggle = () =>{ 
+    SetSidebarToggle(!sidebarToggle);
+    console.log(sidebarToggle)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <Header handleSidebarToggle={handleSidebarToggle} />
+        <div className='app_container border border-info'>
+            {sidebarToggle && <Sidebar sidebar={sidebarToggle} />}
+            <Container fluid className='app__main border border-warning'>
+                <HomeScreen />
+            </Container>
+        </div>
+    </>
+  )
 }
 
-export default App;
+export default App
